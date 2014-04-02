a12ca6a5d70f93997f2dd4c1880c3152a5bd742e
Alex Golubev <lex_golubev@mail.ru>
2014-04-02 16:52:03 +0300

    Transferring file through pthread library and changed code style

--- server.c
+++ server.c
@@ -5,10 +5,36 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <pthread.h>
 
 #define BUF_LENGTH 1000
 #define PORT_NUMBER 8082
 #define MAX_CONNECTIONS 10
+
+#define PTHREAD
+
+void* sendfile(void* param) {
+  int clientSocket = (int) param;
+  int file;
+  ssize_t bytesSent = 1;
+  off_t offset = 0;
+  char buf[BUF_LENGTH];
+  ssize_t bytesRead;
+
+  recv(clientSocket, buf, BUF_LENGTH, 0);
+
+  file = open(buf, O_RDONLY);
+  if (file < 0) {
+    printf("Error while opening file\n");
+  }
+
+  do {
+    bytesRead = read(file, buf, BUF_LENGTH);
+    bytesSent = write(clientSocket, buf, bytesRead);
+  } while (bytesSent > 0);
+  close(file);
+  shutdown(clientSocket, SHUT_RDWR);
+}
 
 int main()
 {
@@ -17,23 +43,19 @@ int main()
     socklen_t addrSize;
     int optval = 1;
     int status;
-    pid_t pid;
     char buf[BUF_LENGTH];
-    ssize_t bytesRead;
 
     memset(buf, 0, BUF_LENGTH * sizeof(char));
 
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket < 0)
-    {
+    if (serverSocket < 0) {
         printf("Error while creating socket\n");
         return 1;
     }
     printf("Socket created successfully\n");
 
     status = setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
-    if (status != 0)
-    {
+    if (status != 0) {
         printf("Error while setting socket options\n");
     }
 
@@ -42,8 +64,7 @@ int main()
     inet_aton("127.0.0.1", &addr.sin_addr.s_addr);
 
     status = bind(serverSocket, (struct sockaddr*)(&addr), sizeof(addr));
-    if (status != 0)
-    {
+    if (status != 0) {
         printf("Error while binding\n");
         return 1;
     }
@@ -56,43 +77,34 @@ int main()
     }
     printf("Server started...\n");
 
-    while (1)
-    {
+    while (1) {
         int clientSocket;
         struct sockaddr_in clientSocketAddr;
 
         clientSocket = accept(serverSocket, (struct sockaddr*)&clientSocketAddr, &addrSize);
-        if (clientSocket < 0)
-        {
+        if (clientSocket < 0) {
             printf("Error while accpeting\n");
             return 1;
         }
 
-        recv(clientSocket, buf, BUF_LENGTH, 0);
-        pid = fork();
-        if (pid == 0)
-        {
-            int file;
-            ssize_t bytesSent = 1;
-            off_t offset = 0;
-
-            file = open(buf, O_RDONLY);
-            if (file < 0)
-            {
-                printf("Error while opening file\n");
-            }
-
-            do {
-		bytesRead = read(file, buf, BUF_LENGTH);
-                bytesSent = write(clientSocket, buf, bytesRead);
-            } while (bytesSent > 0);
-            close(file);
-            shutdown(clientSocket, SHUT_RDWR);
+        int rc;
+        pthread_t clientThread;
+        #ifdef PTHREAD
+        rc = pthread_create(&clientThread, NULL, sendfile, (void*) clientSocket);
+        if (rc){
+          printf("ERROR; return code from pthread_create() is %d\n", rc);
+          return -1;
         }
+        #else
+        pid_t childPid = fork();
+        if (childPid == 0){
+          sendfile((void*)clientSocket);
+          exit(0);
+        }
+        #endif
+      sendfile(buf);
     }
 
     shutdown(serverSocket, SHUT_RDWR);
     return 0;
 }
-
-
